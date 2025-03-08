5a2f2eebd08b39f73d35d55815a3ad4c6462d7b5
dota031 <115540660+dota031@users.noreply.github.com>
2025-03-08 18:07:46 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function openLetter() {
+    document.querySelector(".flap").style.transform = "rotateX(180deg)";
+    document.querySelector(".letter").style.transform = "translateY(0)";
+    document.querySelector(".letter").style.opacity = "1";
+}
