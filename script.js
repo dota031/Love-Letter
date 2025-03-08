d256dc444574098a38be9f1e5a1ec7d9e9ebf027
dota031 <115540660+dota031@users.noreply.github.com>
2025-03-08 18:40:05 +0700

    script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function openLetter() {
     document.querySelector(".letter").style.transform = "translateY(0)";
     document.querySelector(".letter").style.opacity = "1";
 }
+
+// Chơi nhạc khi trang tải xong
+document.addEventListener("DOMContentLoaded", function () {
+    var audio = document.getElementById("music");
+    audio.play().catch(error => console.log("Tự động phát bị chặn, nhấn vào màn hình để phát nhạc!"));
+});
